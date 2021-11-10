6dd4fac5cde2ff18d8a128ac3f0db0628b2eede0
fresco <63629853+choi-woo-sung@users.noreply.github.com>
2021-11-10 16:07:24 +0900

    week1 complete
    week1 complete

--- week 1-Jetpack Compose basics/BasicCodeLab/app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicCodeLab/app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.basiccodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,6 +12,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -48,45 +51,63 @@ class MainActivity : ComponentActivity() {
 //        }
 //    }
 
-    //State hoisting
+
     @Composable
     fun Greeting(name: String) {
         var expanded by remember { mutableStateOf(false) }
 //        val extraPadding = if (expanded.value) 48.dp else 0.dp
 
-        val extraPadding by animateDpAsState(
-            if (expanded) 48.dp else 0.dp,
-            animationSpec = spring(
-                dampingRatio = Spring.DampingRatioMediumBouncy,
-                stiffness = Spring.StiffnessLow
-            )
-        )
+//        val extraPadding by animateDpAsState(
+//            if (expanded) 48.dp else 0.dp,
+//
+//        )
 
-        Surface(
-            color = MaterialTheme.colors.primary,
+        Card(
+            backgroundColor =  MaterialTheme.colors.primary,
             modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
         ) {
             Row(modifier = Modifier.padding(24.dp)) {
                 Column(
                     modifier = Modifier
                         .weight(1f)
-                        .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                        .padding(12.dp)
+                        .animateContentSize(
+                            animationSpec = spring(
+                                dampingRatio = Spring.DampingRatioMediumBouncy,
+                                stiffness = Spring.StiffnessLow
+                            )
+                        )
                 ) {
                     Text(text = "Hello ")
-                    Text(text = "$name!", style = MaterialTheme.typography.h4.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    ))
+                    Text(
+                        text = "$name!", style = MaterialTheme.typography.h4.copy(
+                            fontWeight = FontWeight.ExtraBold
+                        )
+                    )
+                    if (expanded) {
+                        Text(
+                            text = "아기 삼형제 여럿이 풀을뜯고 놀아요  아기 삼형제 여럿이 풀을뜯고 놀아요  아기 삼형제 여럿이 풀을뜯고 놀아요 " +
+                                    " 아기 삼형제 여럿이 풀을뜯고 놀아요  아기 삼형제 여럿이 풀을뜯고 놀아요 "
+                        )
+                    }
                 }
                 IconButton(
                     onClick = {
                         expanded = !expanded
                     }) {
 //                    Text(if (expanded.value) "Show less" else "Show more")
-                    Icon(imageVector = if(expanded)  Icons.Filled , contentDescription = )
+                    Icon(
+                        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                        contentDescription = if (expanded) getString(R.string.show_less) else getString(
+                            R.string.show_more
+                        )
+                    )
                 }
             }
+
         }
     }
+
 
     @Composable
     private fun Greetings(names: List<String> = List(1000) { "$it" }) {
