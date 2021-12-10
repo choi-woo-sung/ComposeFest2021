2b5ee7208a7d8a659102db886627b2c99b6dab3c
fresco <63629853+choi-woo-sung@users.noreply.github.com>
2021-12-10 18:12:59 +0900

    3-3
    finish

---
+++ week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -0,0 +1,8 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.junit4.createComposeRule
+
+class RallyNavHostTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+}
